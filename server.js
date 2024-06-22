af805a3940efea1f81e5fe830976aca300598f6a
afonsobrandaointeli <141183213+afonsobrandaointeli@users.noreply.github.com>
2024-06-22 00:04:37 -0300

    feat: inclui ejs files agora

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const userRoutes = require('./routes/userRoutes');
 const db = require('./config/db');
+const path = require('path');
 
-app.use(express.json());
-app.use('/users', userRoutes);
-
-const PORT = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 db.connect()
   .then(() => {
+    console.log('Conectado ao banco de dados PostgreSQL');
+
+    app.use(express.json());
+
+    const userRoutes = require('./routes/userRoutes');
+    app.use('/users', userRoutes);
+
+    const frontendRoutes = require('./routes/frontRoutes');
+    app.use('/', frontendRoutes);
+
+    // Middleware para lidar com erros de rota não encontrada
+    app.use((req, res, next) => {
+      res.status(404).send('Página não encontrada');
+    });
+
+    // Middleware para lidar com erros internos do servidor
+    app.use((err, req, res, next) => {
+      console.error(err.stack);
+      res.status(500).send('Erro no servidor');
+    });
+
+    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`Servidor rodando na porta ${PORT}`);
     });
